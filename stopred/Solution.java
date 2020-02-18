8e2d63e08bd48e3351acbc16975a3924aa62d11e
thedansilva <43654534+thedansilva@users.noreply.github.com>
2020-02-18 16:50:32 -0500

    many updates
    works on a panic buy/sell basis, using linear regression (specifically ordinary least squares to get before/after slopes):
    - if the price of the stock starts to decline and we own them, sell EVERYTHING before it potentially falls further
    - if the price of a stock starts to decline and we don't own it, buy a couple (literally 2) stocks and have them in our 'stock inventory' so to speak
    - if we're on the last day (1), sell everything (the guy says it autosells on the last day but it doesn't lol)

--- stopred/Solution.java
+++ stopred/Solution.java
@@ -4,12 +4,10 @@ import java.lang.*;
 // idea: half window approach where slope of stocks are evaluated at time positions 3 and time positions 5
 // and if the slope is lower in 2nd eval, sell. if higher, buy.
 public class Solution {
-  static class Stock {
+  public static class Stock implements Comparable<Stock> {
     public String name = " ";
     public int owned = 0;
     public double[] prices = new double [5];
-    public double halfSlope = 0;
-    public double fullSlope = 0;
     public Stock(String n) {
       String[] assign = n.split(" "); // delimit stock data by spaces
       name = assign[0]; // first value is the name of the stock
@@ -18,7 +16,7 @@ public class Solution {
         prices[i] = Double.parseDouble(assign[i + 2]); // assign prices
       }
     }
-    public double halfSlope() { // uses OLS method to get a slope
+    public double halfSlope() { // uses OLS method to get a slope of half the data
       double[] xi = {0, 1, 2};
       double[] yi = prices;
       double xAvg = (xi[0] + xi[1] + xi[2]) / 3;
@@ -36,7 +34,7 @@ public class Solution {
       double m = ExixTyiy / ExixSq;
       return m;
     }
-    public double fullSlope() {
+    public double fullSlope() { // uses OLS method to get a full slope
       double[] xi = {0, 1, 2, 3, 4};
       double[] yi = prices;
       double xAvg = (xi[0] + xi[1] + xi[2] + xi[3] + xi[4]) / 5;
@@ -54,33 +52,68 @@ public class Solution {
       double m = ExixTyiy / ExixSq;
       return m;
     }
-  }
+    public double slopeChange() {
+      return this.fullSlope() - this.halfSlope();
+    }
+    @Override
+    public int compareTo(Stock s2) {
+      return new Double(this.slopeChange()).compareTo(s2.slopeChange());
+    }
+    public boolean risingStock() {
+      if (this.fullSlope() > this.halfSlope()) {
+        return true;
+      }
+      return false;
+    }
+}
 
   public static void main(String[] args) {
       ArrayList<Stock> stocks = new ArrayList<Stock>();
+      ArrayList<String> transactions = new ArrayList<String>();
       Scanner in = new Scanner(System.in);
       String temp = "";
       double m = in.nextDouble();
       int k = in.nextInt();
       int d = in.nextInt();
-      in.nextLine(); // consume unused \n by int d
+      in.nextLine(); // consume unused \n by int d. was causing errors in the next line.
       for (int i = 0; i < k; i++) {
         temp = in.nextLine();
         stocks.add(new Stock(temp));
       }
       // GET THE LINEAR REGRESSION (USING OLS) OF EVERY STOCK UP TO THE HALFWAY POINT OF DAYS
       // GET THE LINEAR REGRESSION (USING OLS) OF EVERY STOCK OF THE ENTIRE LIST OF DAYS
+      // GET A LIST OF MOST RAPIDLY RISING STOCKS BASED ON SLOPE
+      Collections.sort(stocks);
       for (int i = 0; i < stocks.size(); i++) {
-  //      System.out.println("Half slope is " + stocks.get(i).halfSlope());
-  //      System.out.println("Full slope is " + stocks.get(i).fullSlope());
-        for (int j = 0; j < stocks.get(i).prices.length/2 + 1; j++) {
-    //      System.out.println(stocks.get(i).prices[j]);
+      //  System.out.println(stocks.get(i).name + " " + Arrays.toString(stocks.get(i).prices) + ": " + stocks.get(i).slopeChange() + " " + stocks.get(i).risingStock());
+      }
+      if (d == 1) { //one day left, sell everything
+        for (int i = 0; i < stocks.size(); i++) {
+          if (stocks.get(i).owned > 0) {
+            transactions.add(stocks.get(i).name + " SELL " + stocks.get(i).owned);
+          }
+        }
+      } else {
+        for (int i = 0; i < stocks.size(); i++) {
+          if (stocks.get(i).owned > 0 && !(stocks.get(i).risingStock())) {
+            // if our stock starts to fall in value, panic sell all of them
+            transactions.add(stocks.get(i).name + " SELL " + stocks.get(i).owned);
+          }
+          if (stocks.get(i).owned == 0 && !(stocks.get(i).risingStock()) && (stocks.get(i).prices[4] * 2 < m)) {
+          // if there's a stock we don't have and it's nosediving in value, capitalize on that and buy 2 of them if possible
+            transactions.add(stocks.get(i).name + " BUY " + 2);
+            m -= stocks.get(i).prices[4] * 2;
+          }
         }
       }
-      // GET A LIST OF MOST RAPIDLY RISING STOCKS BASED ON SLOPE
+      System.out.println(transactions.size());
+      for (int i = 0; i < transactions.size(); i++) {
+          System.out.println(transactions.get(i));
+    }
       // GET A LIST OF MOST RAPIDLY DECLINING STOCKS BASED ON SLOPE
+      // SELL STOCKS THAT ARE DECLINING
+      // maybe only go every other turn (odd or even days) to better observe stocks?
       // IF WE CAN BUY THE MOST RAPIDLY DECLINING STOCKS, BUY THE TOP 4 STOCKS
       // IF WE CAN SELL THE MOST RAPIDLY RISING STOCKS, SELL THE TOP 4 STOCKS
-      // ON THE FINAL DAY, SELL EVERYTHING
     }
 }
