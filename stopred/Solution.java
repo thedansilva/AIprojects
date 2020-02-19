c1c49b389bea380e896f06a9da16c8f1bf017e33
thedansilva <43654534+thedansilva@users.noreply.github.com>
2020-02-18 20:52:13 -0500

    more stuff
    moved to completely linear regression scheme with the following rules:
    - sell all of the top 3 stocks each turn
    - sell all the stocks we own that have a spike if more than 15 dollars per share each turn
    - buy as much of the most rapidly declining stock at once

--- stopred/Solution.java
+++ stopred/Solution.java
@@ -16,24 +16,6 @@ public class Solution {
         prices[i] = Double.parseDouble(assign[i + 2]); // assign prices
       }
     }
-    public double halfSlope() { // uses OLS method to get a slope of half the data
-      double[] xi = {0, 1, 2};
-      double[] yi = prices;
-      double xAvg = (xi[0] + xi[1] + xi[2]) / 3;
-      double yAvg = (yi[0] + yi[1] + yi[2]) / 3;
-      double Exix = 0;
-      double Eyiy = 0;
-      double ExixTyiy = 0;
-      double ExixSq = 0;
-      for (int i = 0; i < 3; i++) {
-        Exix = xi[i] - xAvg;
-        Eyiy = yi[i] - yAvg;
-        ExixTyiy += Exix * Eyiy;
-        ExixSq += Exix * Exix;
-      }
-      double m = ExixTyiy / ExixSq;
-      return m;
-    }
     public double fullSlope() { // uses OLS method to get a full slope
       double[] xi = {0, 1, 2, 3, 4};
       double[] yi = prices;
@@ -52,18 +34,9 @@ public class Solution {
       double m = ExixTyiy / ExixSq;
       return m;
     }
-    public double slopeChange() {
-      return this.fullSlope() - this.halfSlope();
-    }
     @Override
     public int compareTo(Stock s2) {
-      return new Double(this.slopeChange()).compareTo(s2.slopeChange());
-    }
-    public boolean risingStock() {
-      if (this.fullSlope() > this.halfSlope()) {
-        return true;
-      }
-      return false;
+      return new Double(this.fullSlope()).compareTo(s2.fullSlope());
     }
 }
 
@@ -72,6 +45,7 @@ public class Solution {
       ArrayList<String> transactions = new ArrayList<String>();
       Scanner in = new Scanner(System.in);
       String temp = "";
+      int lowestStock = 0;
       double m = in.nextDouble();
       int k = in.nextInt();
       int d = in.nextInt();
@@ -80,12 +54,18 @@ public class Solution {
         temp = in.nextLine();
         stocks.add(new Stock(temp));
       }
-      // GET THE LINEAR REGRESSION (USING OLS) OF EVERY STOCK UP TO THE HALFWAY POINT OF DAYS
       // GET THE LINEAR REGRESSION (USING OLS) OF EVERY STOCK OF THE ENTIRE LIST OF DAYS
-      // GET A LIST OF MOST RAPIDLY RISING STOCKS BASED ON SLOPE
-      Collections.sort(stocks);
+      // buy low, sell high approach:
+      // whatever stocks has the highest increase in price, sell it.
+      // whatever stock has the highest decrease in price, buy a lot of them.
+      /*
+      Sell all stocks we have for the one with the highest spike.
+Buy all stocks we can for the one with the hardest fall – and hope its price increases in the next days.
+*/
+      Collections.sort(stocks); // get a list of the lowest slopes to highest slopes.
+      Collections.reverse(stocks); // reverse. now we have a list of highest slopes to lowest slopes.
       for (int i = 0; i < stocks.size(); i++) {
-      //  System.out.println(stocks.get(i).name + " " + Arrays.toString(stocks.get(i).prices) + ": " + stocks.get(i).slopeChange() + " " + stocks.get(i).risingStock());
+      //  System.out.println(stocks.get(i).name + " " + Arrays.toString(stocks.get(i).prices) + ": " + stocks.get(i).fullSlope());
       }
       if (d == 1) { //one day left, sell everything
         for (int i = 0; i < stocks.size(); i++) {
@@ -94,22 +74,36 @@ public class Solution {
           }
         }
       } else {
-        for (int i = 0; i < stocks.size(); i++) {
-          if (stocks.get(i).owned > 0 && !(stocks.get(i).risingStock())) {
+        for (int i = 0; i < 3; i++) { // only get the top 3 stocks
+          if (stocks.get(i).owned > 0) {
             // if our stock starts to fall in value, panic sell all of them
             transactions.add(stocks.get(i).name + " SELL " + stocks.get(i).owned);
           }
-          if (stocks.get(i).owned == 0 && !(stocks.get(i).risingStock()) && (stocks.get(i).prices[4] * 2 < m)) {
-          // if there's a stock we don't have and it's nosediving in value, capitalize on that and buy 2 of them if possible
-            transactions.add(stocks.get(i).name + " BUY " + 2);
-            m -= stocks.get(i).prices[4] * 2;
+        }
+        for (int i = 3; i < stocks.size(); i++) {
+          if (stocks.get(i).prices[4] - stocks.get(i).prices[3] >= 15 && stocks.get(i).owned > 0) {
+            // if any other owned stocks show a significant price spike, sell all of them as well
+            transactions.add(stocks.get(i).name + " SELL " + stocks.get(i).owned);
           }
         }
+          Collections.reverse(stocks); // reverse again. now we have regular list of lowest to high.
+          if (stocks.get(0).owned <= 0 && m >= stocks.get(0).prices[4]) {
+            for (double i = m; i > stocks.get(0).prices[4]; i -= stocks.get(0).prices[4]) { // continue to add lowest stocks to buy until we can't buy anymore
+              lowestStock++;
+            }
+            transactions.add(stocks.get(0).name + " BUY " + lowestStock);
+          }
+        //  if (stocks.get(i).owned == 0 && !(stocks.get(i).risingStock()) && (stocks.get(i).prices[4] * 2 < m)) {
+          // if there's a stock we don't have and it's nosediving in value, capitalize on that and buy 2 of them if possible
+        //    transactions.add(stocks.get(i).name + " BUY " + 2);
+          //  m -= stocks.get(i).prices[4] * 2;
+        //  }
+        //}
       }
-      System.out.println(transactions.size());
-      for (int i = 0; i < transactions.size(); i++) {
+        System.out.println(transactions.size());
+        for (int i = 0; i < transactions.size(); i++) {
           System.out.println(transactions.get(i));
-    }
+        }
       // GET A LIST OF MOST RAPIDLY DECLINING STOCKS BASED ON SLOPE
       // SELL STOCKS THAT ARE DECLINING
       // maybe only go every other turn (odd or even days) to better observe stocks?
