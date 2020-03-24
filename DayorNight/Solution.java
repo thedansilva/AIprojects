ff855ae127a1af6856c47c068ee33767dd30fa98
thedansilva <43654534+thedansilva@users.noreply.github.com>
2020-03-24 18:11:54 -0400

    updated; solve rate 15/16 > 16/16

--- DayorNight/Solution.java
+++ DayorNight/Solution.java
@@ -4,68 +4,35 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Solution {
-
-    static int split(String str){
-      int total=0;
-      int average =0;
-      int count=0;
-      String[] arrOfStr = str.split(" ");
-
-          for (String a : arrOfStr){
-
-            String[] pixels = a.split(",");
-
-                for (String p : pixels){
-                    int pnum =Integer.parseInt(p);
-                    total=pnum+total;
-                    count= count+1;
-                    }
-
-              //  System.out.println("count: "+ count);
-          }
-          average= total/count;
-          //System.out.println(total);
-        //  int average= total/pixels.length;
-
-          return average;
-    }
-
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-
-
-//just for testing with file, delete later
-      //   try {
-      // File myObj = new File("testcase3.txt");
-      // Scanner myReader = new Scanner(myObj);
-      //
-      //   String row = myReader.nextLine();
-      //
-      //  System.out.println(row);
-    //  }
-      //myReader.close();
-      int value = split(row);
-  
-
-      if (value>50 ){
-        System.out.println("day");
-      }
-      else{
-        System.out.println("night");
-      }
-
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
-
-
-
-//uncomment to work with STDIN
+        double luminance = 0;
+        double lumeAvg = 0;
+        int count=0;
         Scanner scanner = new Scanner(System.in);
         String row = scanner.nextLine();
         scanner.close();
-        System.out.println(row);
-
+        String[] BGR = row.split(" ");
+        int blue = 0;
+        int green = 0;
+        int red = 0;
+        for(int x = 0; x < BGR.length; x++)
+        {
+                    String[] pixel = BGR[x].split(",");
+                    blue = Integer.parseInt(pixel[0]);
+                    green = Integer.parseInt(pixel[1]);
+                    red = Integer.parseInt(pixel[2]);
+                    luminance = ((0.33*red) + (0.66*green) + (0.33*blue));
+                    lumeAvg += luminance / 255;
+                    count++;
+                    //System.out.println("Blue: " + blue + " Green: " + green + " Red: " + red);
+        }
+        // averaging per pixel
+        lumeAvg /= count;      
+        if (lumeAvg < 0.45)
+            System.out.print("night");
+        else
+            System.out.print("day");
+        
+        //System.out.println("\nLum :" + luminance);
     }
 }
